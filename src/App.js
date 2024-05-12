a77bee953bad629c813024777afd205caae223d4
ehdwo13 <ehdwo13@gmail.com>
2024-05-12 22:32:33 +0900

    20240512

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  let post = "점심 추천 맛집";
-  let[a, b] = useState(['남자 점심 추천','여자 점심 추천','청소년 점심 추천']);
+  let post = "food";
+  let[a, b] = useState(['spagetti','coke','apple']);
   let[likeCount, lc]= useState(0);
   return (
     <div className="App">
@@ -15,16 +15,26 @@ function App() {
           <h4>블로그</h4>
         </div>
         <h4 style={{color : 'red', fontSize : '32px'}}>{post}</h4>
+        <button onClick={()=>{
+          let copy = [...a];
+          copy[0] = 'lemonade';
+          b(copy);
+          }}>변경</button>
+        <button onClick={()=>{
+          let sortCopy = [...a];
+          sortCopy.sort();
+          b(sortCopy);
+        }}>정렬</button>
         <div className='list'>
-          <h4>{a[0]} <span onClick={()=>{lc(likeCount+1)}}> 좋아요</span> {likeCount} </h4>
+          <h4>{a[2]} <span onClick={()=>{lc(likeCount+1)}}> 좋아요</span> {likeCount} </h4>
           <p>5월 11일</p>
         </div>
         <div className='list'>
-          <h4>{a[1]}</h4>
+          <h4>{a[1]} </h4>
           <p>5월 11일</p>
         </div>
         <div className='list'>
-          <h4>{a[2]}</h4>
+          <h4>{a[0]}</h4>
           <p>5월 11일</p>
         </div>
       </div>
