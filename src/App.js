cb6abdedbabfdd64f6c26ae281fdedbfa5dab20e
ehdwo13 <ehdwo13@gmail.com>
2024-05-15 23:40:06 +0900

    20240515

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-
+//좋아요 갯수 개별
 function App() {
   let post = "food";
-  let[a, b] = useState(['spagetti','coke','apple']);
+  let[a, b] = useState(['spagetti','coke','apple','banana']);
   let[likeCount, lc]= useState(0);
+  let [modal, setModal] = useState(false);
   return (
     <div className="App">
-      <div>
         <div className="black-nav">
-          <h4>블로그</h4>
+          <h4>BLOG</h4>
         </div>
         <h4 style={{color : 'red', fontSize : '32px'}}>{post}</h4>
-        <button onClick={()=>{
+        {/* <button onClick={()=>{
           let copy = [...a];
           copy[0] = 'lemonade';
           b(copy);
@@ -34,12 +34,41 @@ function App() {
           <p>5월 11일</p>
         </div>
         <div className='list'>
-          <h4>{a[0]}</h4>
+          <h4 onClick={()=>{setModal(!modal)}}>{a[0]}</h4>
           <p>5월 11일</p>
-        </div>
-      </div>
+        </div> */}
+        {
+          a.map(function(each,i){
+            return (
+              <div className='list' key={i}>
+              <h4 onClick={()=>{setModal(!modal)}}>{each}<span onClick={()=>{lc(likeCount+1)}}> 좋아요</span> {likeCount} </h4>
+              <p>5월 11일</p>
+            </div>
+            )
+          })
+        }
+        {
+          modal == true ?  <Modal/> : null
+        }
     </div>
   );
+}
+
+//하나의 구역(ex = div)만 있어야함 state가져다 못씀
+//컴포넌트
+//반복적인 html 축약
+//큰 페이지들
+//자주 변경되는것들
+function Modal(){
+  return (
+    <>
+      <div className='modal'>
+          <h4>제목</h4>
+          <p>날짜</p>
+          <p>상세내용</p>
+      </div>
+    </>
+  )
 }
 
 export default App;
